92467501018bb2c8db3abb0782cc34719747564d
SPOON <rujipob.c@ku.th>
2024-10-13 15:37:00 +0700

    edit to 0.3

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -28,6 +28,7 @@ export default function MyCard(props) {
   <CardActions>
     <Button size="small">yyy</Button>
     <Button size="small">Learn More</Button>
+    <Button size="small">spoon</Button>
   </CardActions>
 </Card>
 
